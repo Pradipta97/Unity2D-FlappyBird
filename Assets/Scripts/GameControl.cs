5fb06a3ea8c21a26a3157f76eb23cc312b0eaee4
Prad97 <pradiptadasgupta101@outlook.com>
2022-01-19 00:36:57 -0700

    commit55

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+public static GameControl instance;
+
+public GameObject gameOverText;
+
+public bool gameOver = false;
+public float scrollSpeed = -1.5f;
+public Text scoreText;
+
+private int score = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == true && Input.GetMouseButtonDown(0))
+        {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+    }
+
+    public void BirdScore()
+    {
+       
+      if (gameOver)
+      {
+          return;
+      }
+      
+      if(GameObject.Find("Coin") == null)
+      {
+
+      
+         score += 5; 
+      }
+      else 
+      {
+          score++;
+      }
+      
+     scoreText.text = "Score: " + score.ToString();
+    }
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
